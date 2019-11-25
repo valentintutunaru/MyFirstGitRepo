3bbf5a31605f96bc772929d3ff1d616a1e0d7f79
Valentin Tutunaru <tutunaru.valentin@gmail.com>
2019-11-25 21:52:47 +0200

    "Added a new method and a new class car"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public static void doSomething() {
+        System.out.println("Do something cool!");
+    }
 }
